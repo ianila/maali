8d777d1995d3fc80dd686b18fd40d6851dd5e3bb
Anila Senadheera <anilamaali@gmail.com>
2021-05-25 23:35:09 +0530

    setup deploy

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -142,3 +143,6 @@ REST_FRAMEWORK={
 
 # Cors Headers
 CORS_ALLOW_ALL_ORIGINS = True
+
+# Heroku
+django_heroku.settings(locals())
